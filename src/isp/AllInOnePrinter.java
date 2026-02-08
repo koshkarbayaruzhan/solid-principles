1d5c91e3a65cf5793b4e5b41a369e3d92b51164f
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 09:16:09 +0500

    Update AllInOnePrinter.java

--- src/isp/AllInOnePrinter.java
+++ src/isp/AllInOnePrinter.java
@@ -1,4 +1,15 @@
 package isp;
 
-public class AllInOnePrinter {
+public class AllInOnePrinter implements IPrinter, IScanner, IFax {
+    public void print(String content) {
+        System.out.println("Printing: " + content);
+    }
+
+    public void scan(String content) {
+        System.out.println("Scanning: " + content);
+    }
+
+    public void fax(String content) {
+        System.out.println("Faxing: " + content);
+    }
 }
