772208d8fc9403f0868cb67731f8e08483e5e71c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-08 09:18:03 +0500

    Update NotificationService.java

--- src/dip/NotificationService.java
+++ src/dip/NotificationService.java
@@ -1,4 +1,17 @@
 package dip;
 
+import java.util.List;
+
 public class NotificationService {
+    private final List<IMessageSender> senders;
+
+    public NotificationService(List<IMessageSender> senders) {
+        this.senders = senders;
+    }
+
+    public void sendNotification(String message) {
+        for (IMessageSender sender : senders) {
+            sender.send(message);
+        }
+    }
 }
